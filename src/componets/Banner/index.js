171229b35eba8b6dd8d4405e263e6b319a55e611
Nestor <nestor.prado.tenorio@gmail.com>
2024-02-18 10:06:10 -0300

    Made the page banner

---
+++ src/componets/Banner/index.js
@@ -0,0 +1,20 @@
+import { ClassNames } from "@emotion/react";
+import styles from './styles.module.css'
+import Image from 'next/image'
+import Logo from '../../image/lotuslogo.png'
+
+
+
+export default function Banner() {
+    return (
+        <>
+            <div className={styles.banner}>
+            <Image
+                    src={Logo}
+                    alt="Picture of the Lotus Massozen"
+                    className={styles.foto}
+                    /> 
+            </div>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import Header from '../componets/Header';
+import Header from '../componets/Header'
 import Footer from '../componets/Footer'
+import Banner from '../componets/Banner'
 
 export default function Home() {
   return (
     <>
       <main >
         <Header />
+        <Banner/>
         <div>
           <h1>Meio</h1>
         </div>
